4c07f77cb57544d7ec01439c7f8f73e504f7e569
akirby23 <>
2023-11-28 09:40:51 +0000

    Add bolierplate html to the base file

--- patterns/views.py
+++ patterns/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Pattern
 
-# Create your views here.
+
+class PatternList(generic.ListView):
+    model = Pattern
+    queryset = Pattern.objects.filter(status=1).order_by('-created_on')
+    template_name = index.html
+
+
+
