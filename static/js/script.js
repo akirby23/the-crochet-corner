c254eae325d504d41ce8547dbe1581f985b86c47
akirby23 <>
2023-12-02 19:59:40 +0000

    Set up messages, log in & log out functionality

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Times out the alerts after 3 seconds. 
+ * Code borrowed from Code Institute's "I Think Therefore I Blog"
+ * walkthrough tutorial.
+ */
+setTimeout(function () {
+    let messages = document.getElementById("msg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
