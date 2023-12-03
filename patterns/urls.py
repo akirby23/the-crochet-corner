55738e2de295863508569117cd7c58eeb8cd3c14
akirby23 <>
2023-12-03 17:35:04 +0000

    Create views for pattern_list & pattern pages, link up URLs

--- patterns/urls.py
+++ patterns/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PatternList.as_view(), name='home')
+    path('', views.home, name='home'),
+    path('patterns/', views.PatternList.as_view(), name='pattern_list'),
+    path('<slug:slug>/', views.PatternPage.as_view(), name='pattern_page')
 ]
--- patterns/views.py
+++ patterns/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Pattern
+
+
+def home(request):
+    return render(request, 'index.html')
 
 
 class PatternList(generic.ListView):
     model = Pattern
     queryset = Pattern.objects.filter(status=1).order_by('-created_on')
-    template_name = "index.html"
+    template_name = "pattern_list.html"
+
+
+class PatternPage(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Pattern.objects.filter(status=1)
+        pattern = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request, "pattern.html", {
+                "pattern": pattern,
+            },
+        )
