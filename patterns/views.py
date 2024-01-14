f7b5771e81f39f8b9797d317b14c8e74f32db77d
akirby23 <>
2024-01-14 12:25:52 +0000

    Add post method for comment creation

--- patterns/views.py
+++ patterns/views.py
@@ -18,8 +18,33 @@ class PatternPage(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Pattern.objects.filter(status=1)
         pattern = get_object_or_404(queryset, slug=slug)
+        comments = pattern.comments.order_by("-created_on")
         context = {
             "pattern": pattern,
+            "comments": comments,
+            "commented": False,
             "comment_form": CommentForm()
         }
+        return render(request, "pattern.html", context)
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Pattern.objects.filter(status=1)
+        pattern = get_object_or_404(queryset, slug=slug)
+        comments = pattern.comments.order_by("-created_on")
+        context = {
+            "pattern": pattern,
+            "comments": comments,
+            "commented": True,
+            "comment_form": CommentForm()
+        }
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.author = request.user
+            comment = comment_form.save(commit=False)
+            comment.pattern = pattern
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
         return render(request, "pattern.html", context)
